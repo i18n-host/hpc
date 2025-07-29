891003ec72271e9d235626f1cc5748632377dc6f
i18n.host <i18n.host@gmail.com>
2025-07-29 16:26:36 +0800

    ♨

---
+++ coffee/webc/test/vite/Main.svelte
@@ -0,0 +1,20 @@
+<script lang="coffee">
+> svelte > onMount
+
+onMount =>
+  console.log '> hello'
+  return
+
+</script>
+
+<template lang="pug">
+h1 test
+</template>
+
+<style lang="stylus">
+:global(_)
+	overflow hidden
+
+h1
+  color #f00
+</style>
