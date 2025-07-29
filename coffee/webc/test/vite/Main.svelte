637f352dc9842548e2cf041e91c2b940293e1de9
i18n.host <i18n.host@gmail.com>
2025-07-29 18:45:17 +0800

    ♨

--- coffee/webc/test/vite/Main.svelte
+++ coffee/webc/test/vite/Main.svelte
@@ -8,13 +8,27 @@ onMount =>
 </script>
 
 <template lang="pug">
-h1 test
+b
+  h1 面试信息抽取
+  b
+    p 上传面试录音转写的 txt / md 文件
+    p
+      input(type="file" name="file" accept=".txt,.md")
+  p.or
+    b 或者
+  b
+    p 发送飞书文档的连接
+    p
+      input(type="url" name="url" placeholder="飞书文档的链接")
+  hr
+  b.model
+    | 管理能力素质模型
+    a(href="#")
+
 </template>
 
 <style lang="stylus">
-:global(_)
-  overflow hidden
+b
+  font-weight 400
 
-h1
-  color #f00
 </style>
