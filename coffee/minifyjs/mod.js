791676443f35906355bdff05d5d667275b00ee4f
i18n <i18n.site@gmail.com>
2025-08-04 17:58:14 +0800

    ♨

--- coffee/minifyjs/mod.js
+++ coffee/minifyjs/mod.js
@@ -7,57 +7,57 @@ import func2arrow from "./func2arrow.js"
 import { writeFileSync } from "node:fs"
 
 const main = async (code, filename) => {
-  code = putout(code, {
-    plugins: ["esm"],
-    rules: {
-      "esm/inline-export": false,
-      "esm/remove-empty-import": [
-        "off",
-        {
-          ignore: [],
-        },
-      ],
-    },
-    printer: [
-      "putout",
-      {
-        format: {
-          quote: '"',
-        },
-        semantics: {
-          encodeSingleQuote: false,
-          encodeDoubleQuote: true,
-        },
-      },
-    ],
-  }).code
+	code = putout(code, {
+		plugins: ["esm"],
+		rules: {
+			"esm/inline-export": false,
+			"esm/remove-empty-import": [
+				"off",
+				{
+					ignore: [],
+				},
+			],
+		},
+		printer: [
+			"putout",
+			{
+				format: {
+					quote: '"',
+				},
+				semantics: {
+					encodeSingleQuote: false,
+					encodeDoubleQuote: true,
+				},
+			},
+		],
+	}).code
 
-  code = await func2arrow(code)
-  const r = await minify(code, {
-    compress: {
-      toplevel: true, // 启用顶层优化
-      unused: true, // 删除未导出变量
-      defaults: true, // 启用所有压缩选项
-      drop_console: true,
-      hoist_funs: true,
-      hoist_vars: true,
-      booleans_as_integers: true,
-      reduce_funcs: true,
-      unsafe: true,
-    },
-    mangle: {
-      toplevel: true, // 缩短顶层变量名
-    },
-    sourceMap: true,
-    ecma: 2022,
-    module: true,
-  }),
-    map = JSON.parse(r.map)
+	code = await func2arrow(code)
+	const r = await minify(code, {
+			compress: {
+				toplevel: true, // 启用顶层优化
+				unused: true, // 删除未导出变量
+				defaults: true, // 启用所有压缩选项
+				drop_console: true,
+				hoist_funs: true,
+				hoist_vars: true,
+				booleans_as_integers: true,
+				reduce_funcs: true,
+				unsafe: true,
+			},
+			mangle: {
+				toplevel: true, // 缩短顶层变量名
+			},
+			sourceMap: true,
+			ecma: 2022,
+			module: true,
+		}),
+		map = JSON.parse(r.map)
 
-  map.sources[0] = filename
+	map.sources[0] = filename
 
-  r.map = JSON.stringify(map)
-  return r
+	r.map = JSON.stringify(map)
+	return r
 }
 
 export default main
